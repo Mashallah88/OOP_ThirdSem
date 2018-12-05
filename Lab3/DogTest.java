28d0e11991292310288ad68b1319db933b23ac46
Mashallah88 <32356001+Mashallah88@users.noreply.github.com>
2018-12-05 18:43:58 +0300

    Create DogTest.java

---
+++ Lab3/DogTest.java
@@ -0,0 +1,13 @@
+public class DogTest
+{
+    public static void main(String[] args)
+    {
+        Havanese Krishti = new Havanese(31.5,4.7);
+        Dachshund Fred = new Dachshund (42, 9.8, 70);
+        Fred.play();
+        Krishti.bark();
+        Fred.fox();
+        Krishti.beCute();
+
+    }
+}
