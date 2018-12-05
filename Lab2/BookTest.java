e00c16b4c22f67d437bf8dc081426b51806c2a47
Mashallah88 <32356001+Mashallah88@users.noreply.github.com>
2018-12-05 18:42:26 +0300

    Create BookTest.java

---
+++ Lab2/BookTest.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class BookTest
+{
+    public static void main(String[] args)
+    {
+        //Установка через конструктор
+        Book first_book = new Book ("А.С. Пушкин", "Евгений Онегин", 1833, 239);
+
+        int pages = first_book.getPages();
+        if (pages < 300)
+            System.out.println("В этой книге меньше трехсот страниц");
+        else
+            System.out.println("В этой книге больше трехсот страниц");
+
+        //Вывод чрез метод
+        first_book.bInfo();
+
+        System.out.println("Введите новое количество страниц");
+        final Scanner in = new Scanner(System.in);
+        first_book.setPages(in.nextInt());
+
+        //Вывод через метод
+        first_book.bInfo();
+
+        Book second_book = new Book();
+        second_book.bInfo();
+        
+        //Вывод через system
+        System.out.println(first_book);
+        in.close();
+    }
+}
