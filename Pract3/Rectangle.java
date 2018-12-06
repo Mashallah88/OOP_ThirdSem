c23dfd007df59b975a39aba46284eac7ef39fceb
Mashallah88 <32356001+Mashallah88@users.noreply.github.com>
2018-12-06 10:34:25 +0300

    Create Rectangle.java

---
+++ Pract3/Rectangle.java
@@ -0,0 +1,62 @@
+public class Rectangle extends Shape 
+{
+    protected double wigth;
+    protected double length;
+
+    public Rectangle()
+  {
+        this.color = "red";
+        this.filled = false;
+        this.wigth = 1;
+        this.length = 2;
+    }
+
+    public Rectangle(double wigth, double length)
+  {
+        this.color = "red";
+        this.filled = false;
+        this.wigth = wigth;
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double wigth, double length)
+  {
+        this.color = color;
+        this.filled = filled;
+        this.wigth = wigth;
+        this.length = length;
+    }
+
+    public void setWigth(double w)
+   {
+        this.wigth = w;
+    }
+    public double getWigth()
+   {
+        return wigth;
+    }
+
+    public void setLength(double l)
+   {
+        this.length = l;
+    }
+    public double getLength()
+   {
+        return length;
+    }
+
+    public double getArea()
+   {
+        return wigth*length;
+    }
+
+    public double getPerimeter()
+   {
+        return 2*wigth+2*length;
+    }
+
+    public String toString()
+   {
+        return "Shape: rectangle, wigth: " +wigth+ ", length: " +length+ ", color: " +color;
+    }
+}
